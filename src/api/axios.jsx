7c939ecd12aa9b2c8aea77eac1fb290505eb1ab9
Muhammad Ikram <ikramkamboh818@gmail.com>
2026-01-10 13:05:35 +0500

    Build ready and successful

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,3 +1,13 @@
+import axios from 'axios';
+// const API = axios.create({
+//   // baseURL: "https://fleet-watch.onrender.com", 
+//   baseURL: 'http://localhost:5000/api',
+// });
+
 const API = axios.create({
-  baseURL: "https://fleet-watch.onrender.com", 
+  // Live hone par ye Render ka URL use karega
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    baseURL: "https://fleet-watch.onrender.com", 
+
 });
+export default API;
