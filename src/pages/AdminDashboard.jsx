3b3fdac27f42677933ddb7a6a391525d3a7dd878
Muhammad Ikram <ikramkamboh818@gmail.com>
2026-01-15 13:45:18 +0500

    updated code

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -301,7 +301,7 @@ const AdminDashboard = () => {
         <div className="d-flex align-items-center mb-4 p-3 glass-card shadow-sm animate__animated animate__fadeInDown">
 
           <h5 className="mb-0 text-white fw-light">FleetWatch / <span className="fw-bold text-info" style={{ textTransform: "capitalize" }}>{activeTab}</span></h5>
-
+          <h6 style={gray}>Login as an Admin</h6>
         </div>
 
 
